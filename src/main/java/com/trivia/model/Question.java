5dad85515b5f95efb24e6c99ff27501df1fb6981
Yaser Tirsen <yaser.tirsen@mydit.ie>
2019-11-29 22:51:06 +0000

    created Question model

---
+++ src/main/java/com/trivia/model/Question.java
@@ -0,0 +1,55 @@
+package com.trivia.model;
+
+import javax.persistence.*;
+import javax.print.DocFlavor;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "questions")
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "category", nullable = false)
+    private String question;
+    private ArrayList<String> choices;
+    private String correctAnswer;
+
+    public Question() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public ArrayList<String> getChoices() {
+        return choices;
+    }
+
+    public void setChoices(ArrayList<String> choices) {
+        this.choices = choices;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+}
---
+++ src/main/java/com/trivia/repository/QuestionRepository.java
@@ -0,0 +1,9 @@
+package com.trivia.repository;
+
+import com.trivia.model.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+}
